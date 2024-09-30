436449cde724eaff2184f56565b1fd12d27beeb3
Desmond Egya <egyadesmond@gmail.com>
2024-09-30 22:20:03 +0100

    Build the login component.

---
+++ src/components/Login.tsx
@@ -0,0 +1,38 @@
+// src/components/Login.tsx
+import React from 'react';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Text,
+  Link,
+} from '@chakra-ui/react';
+
+const Login: React.FC = () => {
+  return (
+    <Box width="400px" mx="auto" mt="100px" p="5" borderWidth="1px" borderRadius="lg">
+      <Text fontSize="2xl" mb="4" textAlign="center">Login</Text>
+      <form>
+        <Stack spacing={4}>
+          <FormControl isRequired>
+            <FormLabel>Email</FormLabel>
+            <Input type="email" placeholder="Enter your email" />
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel>Password</FormLabel>
+            <Input type="password" placeholder="Enter your password" />
+          </FormControl>
+          <Button type="submit" colorScheme="teal" width="full">Login</Button>
+        </Stack>
+      </form>
+      <Text mt="4" textAlign="center">
+        Don't have an account? <Link color="teal.500" href="/signup">Sign up</Link>
+      </Text>
+    </Box>
+  );
+};
+
+export default Login;
